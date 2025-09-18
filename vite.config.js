e717f72bbe56e2fe623f655f495bd4dddc5711b4
kyuubi-Yhp <south01village@gmail.com>
2025-09-19 02:06:31 +0300

    fix rename deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactFormRegistration/',
+  base: '/ReactFormRegistration0.2.0/',
 })
